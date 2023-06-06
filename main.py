b7bd2d195722bc00fce8a49e8f6ad83c35ed1d30
JuliaRoughley <roughleyjulia@gmail.com>
2023-06-06 09:58:01 +0100

    Updates to main.py to account for using either JSON or CSV

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from movie_app import MovieApp
 from storage_JSON import StorageJson
+from storage_csv import StorageCSV
+from movie_app import MovieApp
 
 
 def main():
-    storage = StorageJson('data\\movies.json')
+    user_storage = input("Are you using 'JSON' or 'CSV?'")
+    if user_storage == "JSON":
+        storage = StorageJson('data\\movies.json')
+    elif user_storage == "CSV":
+        storage = StorageCSV('movies.csv')
     app = MovieApp(storage)
     app.run()
 
